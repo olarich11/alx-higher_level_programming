2a30c956fedcf990de461cfdc4037e543d151203
olarich11 <olarich11@gamil.com>
2023-08-09 01:21:02 -0700

    print_hexa fixed

--- 0x01-python-if_else_loops_functions/4-print_hexa.py
+++ 0x01-python-if_else_loops_functions/4-print_hexa.py
@@ -1,7 +1,4 @@
 #!/usr/bin/python3
 
 for num in range(0, 99):
-    if num != 98:
-        print("{:d} = 0x{:x}".format(num, num))
-    else:
-        print("{:d} = 0x{:x}".format(num, num), end='\n')
+    print("{:d} = 0x{:x}".format(num, num))
