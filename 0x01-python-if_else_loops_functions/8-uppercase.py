53ae8d469a29969b1c2f4d5a141e973b6572b8c2
olarich11 <olarich11@gamil.com>
2023-08-09 01:49:02 -0700

    uppercase

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python3
-def to_uper(character):
+
+def to_upper(character):
     if ord(character) >= 97 and ord(character) <= 122:
         return chr(ord(character) - 32)
     else:
         return character
 
+
 def uppercase(string):
     string_new = ""
-    for char in string:
-        string_new += to_uper(char)
-    return string_new
