912f53e1142f5e2f9441224d18324e210a2a4369
olarich11 <olarich11@gamil.com>
2023-08-10 13:05:11 -0700

    Tasks 0

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -5,7 +5,6 @@ if __name__ == "__main__":
     a = 1
     b = 2
 
-    from add_0 import add as add_0
-    result = add_0(a, b)
+    from add_0 import add
 
-    print("{:d} + {:d} = {:d}".format(a, b, result(a, b)))
+    print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
