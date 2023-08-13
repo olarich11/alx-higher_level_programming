0316ad679b9ef8451e881f05e311cd4a0c614109
olarich11 <olarich11@gamil.com>
2023-08-13 09:53:55 -0700

    Task 9

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,9 @@
+def max_integer(my_list=[]):
+    if len(my_list) == 0:
+        return None
+    else:
+        count = my_list[0]
+        for i in range(len(my_list)):
+            if my_list[i] > count:
+                count = my_list[i]
+        return count
