e0d265c7944021d56c9e46579b93c44a79bf7fca
olarich11 <olarich11@gamil.com>
2023-08-16 09:58:04 -0700

    Tasks 10

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,13 @@
+def best_score(a_dictionary):
+    if not a_dictionary:
+        return None
+
+    max_score = -1
+    best_student = None
+
+    for student, score in a_dictionary.items():
+        if score > max_score:
+            max_score = score
+            best_student = student
+
+    return best_student
