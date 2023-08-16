61f6cd89f00e8bf8848a2656b82674f651eb20d5
olarich11 <olarich11@gamil.com>
2023-08-16 14:43:49 -0700

    Task Advanced

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
+
 def weight_average(my_list=[]):
+    
     if not my_list:
         return 0
 
@@ -9,6 +11,9 @@ def weight_average(my_list=[]):
     for tup in my_list:
         num += tup[0] * tup[1]
         den += tup[1]
+
+    if den == 0:
+        return 0
 
     
     return num / den
