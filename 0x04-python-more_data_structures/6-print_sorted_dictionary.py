9d294cdfed289f2672aeae59611987919299909a
olarich11 <olarich11@gamil.com>
2023-08-16 09:26:39 -0700

    Tasks 6

---
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -0,0 +1,5 @@
+def print_sorted_dictionary(a_dictionary):
+    sorted_keys = sorted(a_dictionary.keys())
+    for key in sorted_keys:
+        value = a_dictionary[key]
+        print("{}: {}".format(key, value))
