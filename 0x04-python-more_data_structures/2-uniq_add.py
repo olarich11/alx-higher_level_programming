acd498a5501f0ab148dd3949a7b7abd8ec5f9624
olarich11 <olarich11@gamil.com>
2023-08-16 13:54:34 -0700

    Task 2

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,17 +1,11 @@
 #!/usr/bin/python3
-
 def uniq_add(my_list=[]):
-    uniq_list = []
-    num = 0
+    unique_elements = []
+    total_sum = 0
 
-    for i in my_list:
-        if i not in uniq_list:
-            uniq_list.append(i)
-            num += i
+    for num in my_list:
+        if num not in unique_elements:
+            unique_elements.append(num)
+            total_sum += num
 
-    return num
-
-if __name__ == "__main__":
-    my_list = [1, 2, 2, 3, 4, 4, 5]
-    result = uniq_add(my_list)
-    print("Result:", result)
+    return total_sum
