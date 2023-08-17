a9f397cfbaf0486c8f4c09a5e44ea3795c4d8182
olarich11 <olarich11@gamil.com>
2023-08-17 14:48:56 -0700

    Task 0

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -7,12 +7,19 @@
  * @h: head of the list
  * Return: the number of nodes
  */
-size_t print_dlistint(const dlistint_t *h)
-{
-	size_t count = 0;
+size_t print_dlistint(const dlistint_t *h) {
+
+	unsigned int count = 0;
 	
-	while (h != NULL)
-	{
+	if (h == NULL) {
+		return (0);
+	}
+	
+	while (h->prev != NULL) {
+		h = h->prev;
+	}
+	
+	while (h != NULL) {
 		printf("%d\n", h->n);
 		count++;
 		h = h->next;
