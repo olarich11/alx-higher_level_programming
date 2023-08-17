479e24658cdd7cfb83ca4c45c59d31d66be2d7c9
olarich11 <olarich11@gamil.com>
2023-08-17 15:26:31 -0700

    Task 0

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -10,22 +10,15 @@
  */
 size_t print_dlistint(const dlistint_t *h)
 {
-	int count;
+	size_t nodes = 0;
 
-	count = 0;
-
-	if (h == NULL)
-		return (count);
-
-	while (h->prev != NULL)
-		h = h->prev;
-
-	while (h != NULL)
+	while h
 	{
+		node++;
 		printf("%d\n", h->n);
-		count++;
+
 		h = h->next;
 	}
-
-	return (count);
+	return (nodes);
 }
+
