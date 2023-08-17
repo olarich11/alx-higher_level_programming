0d189fba3c8a0cd220503c108d6a11c9d544b7a8
olarich11 <olarich11@gamil.com>
2023-08-16 21:43:06 -0700

    Tasks 102

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -1,3 +1,11 @@
 #!usr/bin/python3
-def complex_delete(a_dictionary, value):
-    return {key: val for key, val in a_dictionary.items() if val != value}
+def complex_delete(my_dict, value):
+    keys_to_remove = []
+    for key, val in my_dict.items():
+        if val == value:
+            keys_to_remove.append(key)
+
+    for key in keys_to_remove:
+        my_dict.pop(key)
+
+    return my_dict
