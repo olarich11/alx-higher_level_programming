15c623cc9a1ca4703e69ded87d838a239d9bbd60
olarich11 <olarich11@gamil.com>
2023-08-28 08:28:50 -0700

    Task 2

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 
+
 def safe_print_list_integers(my_list=[], x=0):
     ret = 0
     for i in range(0, x):
@@ -8,5 +9,5 @@ def safe_print_list_integers(my_list=[], x=0):
             ret += 1
         except (ValueError, TypeError):
             continue
-    print("");
-    return ret
+    print("")
+    return (ret)
