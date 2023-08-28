867ffcdf357227495ed49fc0fc4b6ae676d83813
olarich11 <olarich11@gamil.com>
2023-08-28 07:52:14 -0700

    Task 8

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -5,6 +5,6 @@ def safe_function(fct, *args):
     try:
         result = fct(*args)
         return result
-    except Exception as i:
+    except Exception as e:
         print("Exception: {}".format(e), file=sys.stderr)
         return None
