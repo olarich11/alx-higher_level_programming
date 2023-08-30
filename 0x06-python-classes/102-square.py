f162b796b39aa75a25a6713328fbccdd8e4cc4e0
olarich11 <olarich11@gamil.com>
2023-08-29 20:17:37 -0700

    Tasks 9checked

--- 0x06-python-classes/102-square.py
+++ 0x06-python-classes/102-square.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 """Define a class Square."""
 
+
 class Square:
     """Represent a square."""
-
 
     def __init__(self, size=0):
         """Initialize a new square.
@@ -12,12 +12,10 @@ class Square:
         """
         self.size = size
 
-
     @property
     def size(self):
         """Get/set the current size of the square."""
         return (self.__size)
-
 
     @size.setter
     def size(self, value):
@@ -27,36 +25,29 @@ class Square:
             raise ValueError("size must be >= 0")
         self.__size = value
 
-
     def area(self):
         """Return the current area of the square."""
         return (self.__size * self.__size)
-
 
     def __eq__(self, other):
         """Define the == comparision to a Square."""
         return self.area() == other.area()
 
-
     def __ne__(self, other):
         """Define the != comparison to a Square."""
         return self.area() != other.area()
-
 
     def __lt__(self, other):
         """Define the < comparison to a Square."""
         return self.area() < other.area()
 
-
     def __le__(self, other):
         """Define the <= comparison to a Square."""
         return self.area() <= other.area()
 
-
     def __gt__(self, other):
         """Define the > comparison to a Square."""
         return self.area() > other.area()
-
 
     def __ge__(self, other):
         """Define the >= compmarison to a Square."""
