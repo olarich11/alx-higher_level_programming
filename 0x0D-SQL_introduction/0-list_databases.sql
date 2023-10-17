ca49c6f9be14bd30918569e84efaa131dc5e4fcf
olarich11 <olarich11@gamil.com>
2023-10-17 06:51:00 -0700

    Create a database

---
+++ 0x0D-SQL_introduction/0-list_databases.sql
@@ -0,0 +1,2 @@
+-- Display a list of all the databases of my MySQL server.
+SHOW DATABASES;
---
+++ 0x0D-SQL_introduction/1-create_database_if_missing.sql
@@ -0,0 +1,2 @@
+-- Generate the hbtn_0c_0 database on my MySQL server.
+CREATE TABLE DATABASE IF NOT EXISTS `hbtn_0c_0`;
