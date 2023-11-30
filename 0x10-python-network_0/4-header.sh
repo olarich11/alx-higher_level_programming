29656bd29bc4da7a6528fac394cc2995b8f712cd
olarich11 <olarich11@gamil.com>
2023-11-30 13:24:31 -0800

    updated cURL headers

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Perform a GET request to a specified URL with a custom header variable.
+# Issue a GET request to a specified URL and show the response status code.
 curl -sH "X-HolbertonSchool-User-Id: 98" "${1}"
