aacba89d6f732d77ba6d486bc3105c3ce7a1d2b6
olarich11 <olarich11@gamil.com>
2023-12-01 14:02:16 -0800

    updated What's my status

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,19 +1,10 @@
 #!/usr/bin/python3
-"""
-Module that utilizes "request" library to fetch the status of https://intranet.hbtn.io.
-"""
+"""fetches https://intranet.hbtn.io/status."""
 import requests
 
 
-def main():
-    """
-    Function that fetches https://intranet.hbtn.io/status
-    """
-    url = 'https://intranet.hbtn.io/status'
-    r = requests.get(url)
-    print('Body response:')
-    print('\t- type: {}'.format(type(r.text)))
-    print('\t- content: {}'.format(r.text))
-
 if __name__ == "__main__":
-    main()
+    r = requests.get("https://intranet.hbtn.io/status")
+    print("Body response:")
+    print("\t- type: {}".format(type(r.text)))
+    print("\t- content: {}".format(r.text))
